dc6df5551cc333fdde35c511b99b5bef3288394c
лукманхакимд <@223-16>
2022-08-29 20:42:39 +0600

    Добавьте файлы проекта.

---
+++ Lesson4.SEP212/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4.SEP212
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.DetailCars = "panel";
+            Console.WriteLine(car.DetailCars);
+        }
+    }
+    internal class Car
+    {
+
+        public string DetailCars { get; set; }
+        public Car()
+        {
+
+        }
+        public Car(string detailCars)
+        {
+            this.DetailCars = detailCars;
+        }
+
+    }
+}
